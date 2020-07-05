15b15da0fcffef55f194a09282ac0e3f4725129a
YunusNS <yunusw718@gmail.com>
2020-07-05 10:07:58 +0800

    change media player provider

--- app/src/main/java/id/nyaa/tesvideoplayerii/player/PlayerActivity.java
+++ app/src/main/java/id/nyaa/tesvideoplayerii/player/PlayerActivity.java
@@ -1,48 +1,89 @@
 package id.nyaa.tesvideoplayerii.player;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.universalvideoview.UniversalMediaController;
+import com.universalvideoview.UniversalVideoView;
+
+import java.io.IOException;
 
 import id.nyaa.tesvideoplayerii.R;
 import id.nyaa.tesvideoplayerii.bean.LocalVideoBean;
 import id.nyaa.tesvideoplayerii.bean.Resources;
 
-public class PlayerActivity extends AppCompatActivity{
+public class PlayerActivity extends AppCompatActivity implements UniversalVideoView.VideoViewCallback {
     VideoView videoView;
     int position = -1;
 
     private id.nyaa.tesvideoplayerii.bean.Resources mVideoRes;
-    public  boolean isPlayLocalVideo = false;
+    public boolean isPlayLocalVideo = false;
 
+    private static final String TAG = "MainActivity";
+    private static final String SEEK_POSITION_KEY = "SEEK_POSITION_KEY";
+    private static final String VIDEO_URL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+
+    View mBottomLayout;
+    View mVideoLayout;
+    TextView mStart;
+    UniversalVideoView mVideoView;
+    UniversalMediaController mMediaController;
+
+    private int mSeekPosition;
+    private int cachedHeight;
+    private boolean isFullscreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
 
-        videoView = findViewById(R.id.myPlayer);
+        mVideoLayout = findViewById(R.id.video_layout);
+        mBottomLayout = findViewById(R.id.bottom_layout);
+        mVideoView = (UniversalVideoView) findViewById(R.id.videoView);
+        mMediaController = (UniversalMediaController) findViewById(R.id.media_controller);
+        mVideoView.setMediaController(mMediaController);
+        setVideoAreaSize();
+        mVideoView.setVideoViewCallback(this);
+        mStart = (TextView) findViewById(R.id.start);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (mSeekPosition > 0) {
+            mVideoView.seekTo(mSeekPosition);
+        }
+        mVideoView.start();
 
-        position = getIntent().getIntExtra("position", -1);
-        //getSupportActionBar().hide();
-
-
-        playerVideo();
-
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+            }
+        });
     }
 
-    private void playerVideo() {
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mVideoView != null && mVideoView.isPlaying()) {
+            mSeekPosition = mVideoView.getCurrentPosition();
+            mVideoView.pause();
+        }
+    }
+
+    private void setVideoAreaSize() {
 
         int videoType = getIntent().getIntExtra("videoType", -1);
         if (videoType == 0) {
             mVideoRes = getIntent().getParcelableExtra("video");
-        } else if(videoType == 1){
+        } else if (videoType == 1) {
             //Local video
             isPlayLocalVideo = true;
             LocalVideoBean video = (LocalVideoBean) getIntent()
@@ -52,35 +93,96 @@ public class PlayerActivity extends AppCompatActivity{
             mVideoRes.setTitle(video.title);
             mVideoRes.setDescription("");
             mVideoRes.setLink(video.path);
+            mMediaController.setTitle(video.title);
         }
 
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-
-        videoView.setMediaController(mediaController);
-        videoView.setVideoPath(String.valueOf(Uri.parse(mVideoRes.getLink())));
-        videoView.requestFocus();
-
-        videoView.setOnPreparedListener(new android.media.MediaPlayer.OnPreparedListener() {
+        mVideoLayout.post(new Runnable() {
             @Override
-            public void onPrepared(android.media.MediaPlayer mp) {
-                videoView.start();
+            public void run() {
+                int width = mVideoLayout.getWidth();
+                cachedHeight = (int) (width * 405f / 720f);
+//                cachedHeight = (int) (width * 3f / 4f);
+//                cachedHeight = (int) (width * 9f / 16f);
+                ViewGroup.LayoutParams videoLayoutParams = mVideoLayout.getLayoutParams();
+                videoLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                videoLayoutParams.height = cachedHeight;
+                mVideoLayout.setLayoutParams(videoLayoutParams);
+
+                mVideoView.setVideoPath(String.valueOf(Uri.parse(mVideoRes.getLink())));
+                mVideoView.requestFocus();
             }
         });
+    }
 
-        videoView.setOnCompletionListener(new android.media.MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(android.media.MediaPlayer mp) {
-                //videoView.setVideoPath(String.valueOf(MainActivity.fileArrayList.get(position = position + 1)));
-                //videoView.start();
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState Position=" + mVideoView.getCurrentPosition());
+        outState.putInt(SEEK_POSITION_KEY, mSeekPosition);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle outState) {
+        super.onRestoreInstanceState(outState);
+        mSeekPosition = outState.getInt(SEEK_POSITION_KEY);
+        Log.d(TAG, "onRestoreInstanceState Position=" + mSeekPosition);
+    }
+
+
+    @Override
+    public void onScaleChange(boolean isFullscreen) {
+        this.isFullscreen = isFullscreen;
+        if (isFullscreen) {
+            ViewGroup.LayoutParams layoutParams = mVideoLayout.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            mVideoLayout.setLayoutParams(layoutParams);
+            mBottomLayout.setVisibility(View.GONE);
+
+        } else {
+            ViewGroup.LayoutParams layoutParams = mVideoLayout.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            layoutParams.height = this.cachedHeight;
+            mVideoLayout.setLayoutParams(layoutParams);
+            mBottomLayout.setVisibility(View.VISIBLE);
+        }
+
+        switchTitleBar(!isFullscreen);
+    }
+
+    private void switchTitleBar(boolean show) {
+        androidx.appcompat.app.ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            if (show) {
+                supportActionBar.show();
+            } else {
+                supportActionBar.hide();
             }
-        });
+        }
+    }
 
+    @Override
+    public void onPause(MediaPlayer mediaPlayer) {
+    }
+
+    @Override
+    public void onStart(MediaPlayer mediaPlayer) {
+    }
+
+    @Override
+    public void onBufferingStart(MediaPlayer mediaPlayer) {
+    }
+
+    @Override
+    public void onBufferingEnd(MediaPlayer mediaPlayer) {
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        videoView.stopPlayback();
+        if (this.isFullscreen) {
+            mVideoView.setFullscreen(false);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
